c9932756551876d569ffa2c95c95c8063a693c7a
jesusslara <89094107+jesusslara@users.noreply.github.com>
2026-04-16 00:32:00 -0500

    Update lc3vm.h

--- include/lc3vm.h
+++ include/lc3vm.h
@@ -65,7 +65,8 @@ extern uint16_t mem[];
 extern uint16_t reg[];
 extern uint16_t PC_START;
 
-// your task functions should go here
+uint16_t mem_read(uint16_t addr);
+void mem_write(uint16_t addr, uint16_t val);
 
 void rti(uint16_t i);
 void res(uint16_t i);
