6018914a7cc6aa24adf8fe64ed5de12c416de177
jesusslara <89094107+jesusslara@users.noreply.github.com>
2026-04-16 00:42:14 -0500

    Update lc3vm.c

--- src/lc3vm.c
+++ src/lc3vm.c
@@ -46,6 +46,9 @@ uint16_t PC_START = 0x3000;
  *   simply reads and returns the 16 bits stored at the indicated address.
  */
 // put your implememtation of mem_read() here below it documentation
+uint16_t mem_read(uint16_t address) {
+    return mem[address];
+}
 
 /** @brief memory write, transfer to memory
  *
@@ -63,6 +66,9 @@ uint16_t PC_START = 0x3000;
  *   character, or some other type of data.
  */
 // put your implememtation of mem_write() here below it documentation
+void mem_write(uint16_t address, uint16_t value) {
+    mem[address] = value;
+}
 
 /** @brief sign extend bits
  *
